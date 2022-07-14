dcd699b41e2a8ff6efcab41c598b3e5ef2ee6db2
Tomat <xxlennygamerxx@gmail.com>
2022-07-14 15:44:34 -0700

    v1.0.3, fixed multiplayer syncing and made it work in newer versions of tML.

--- RecipeShuffler.cs
+++ RecipeShuffler.cs
@@ -1,26 +1,36 @@
 using System.Collections.Generic;
+using System.IO;
 using RecipeShuffler.Cache;
 using Terraria.ModLoader;
 
 namespace RecipeShuffler
 {
-	public class RecipeShuffler : Mod
-	{
-		/// <summary>
-		///		Represents the Recipe cache in its vanilla state. Should be references as the "natural" state of recipes.
-		/// </summary>
-		/// <remarks>
-		///		Useful for re-shuffling recipes between worlds.
-		/// </remarks>
-		public RecipeCache VanillaCache { get; protected set; } = null!; // Never null when used.
+    public class RecipeShuffler : Mod
+    {
+        /// <summary>
+        ///		Represents the Recipe cache in its vanilla state. Should be referenced as the "natural" state of recipes.
+        /// </summary>
+        /// <remarks>
+        ///		Useful for re-shuffling recipes between worlds.
+        /// </remarks>
+        public RecipeCache VanillaCache { get; protected set; } = null!; // Never null when used.
 
-		public readonly Dictionary<int, RecipeCache> Caches = new();
+        public PacketHandler PacketHandler;
 
-		public override void PostAddRecipes()
-		{
-			base.PostAddRecipes();
+        public readonly Dictionary<int, RecipeCache> Caches = new();
 
-			VanillaCache = new VanillaRecipeCache();
-		}
-	}
+        public RecipeShuffler()
+        {
+            PacketHandler = new(this);
+        }
+
+        public override void PostAddRecipes()
+        {
+            base.PostAddRecipes();
+
+            VanillaCache = new VanillaRecipeCache();
+        }
+
+        public override void HandlePacket(BinaryReader reader, int whoAmI) => PacketHandler.HandlePacket(reader, whoAmI);
+    }
 }
--- WorldRecipeRandomizer.cs
+++ WorldRecipeRandomizer.cs
@@ -1,23 +1,66 @@
 ﻿using RecipeShuffler.Cache;
+using ReLogic.Utilities;
+using System;
+using System.Diagnostics;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+
 
 namespace RecipeShuffler
 {
     public class WorldRecipeRandomizer : ModPlayer
     {
-        private RecipeShuffler Shuffler => (RecipeShuffler) Mod;
+        protected RecipeShuffler Shuffler => (RecipeShuffler)Mod;
+
+        protected Func<bool>? Task = null;
 
         public override void OnEnterWorld(Player player)
         {
-            void Log(string msg) => Main.NewText($"[{Mod.Name}] {msg}");
+            if (!AppliesToUs(player)) return;
 
-            if (!int.TryParse(WorldGen.currentWorldSeed, out int seed))
+            if (Main.netMode == NetmodeID.MultiplayerClient)
             {
-                Log($"Could not parse world seed \"{seed}\", aborting recipe shuffling.");
-                return;
+                WorldGen.currentWorldSeed = "";
+
+                // Send request packet to the server.
+                ModPacket packet = Shuffler.GetPacket();
+                packet.Write((byte)PacketHandler.PacketType.RequestSeedFromServer);
+                packet.Send();
+
+                Task = () =>
+                {
+                    if (string.IsNullOrEmpty(WorldGen.currentWorldSeed)) return false;
+
+                    ShuffleRecipes();
+                    return true;
+                };
             }
-            
+            else if (Main.netMode == NetmodeID.SinglePlayer) ShuffleRecipes();
+        }
+
+        public override void PreUpdate()
+        {
+            if (AppliesToUs(Player) && Task != null && Task()) Task = null;
+        }
+
+        protected bool AppliesToUs(Player player)
+        {
+            return Main.netMode == NetmodeID.SinglePlayer || (Main.netMode == NetmodeID.MultiplayerClient && player.whoAmI == Main.myPlayer);
+        }
+
+        protected void ShuffleRecipes()
+        {
+            void Log(string msg)
+            {
+                Main.NewText($"[{Mod.Name}] {msg}");
+                Shuffler.Logger.Debug($"[{Mod.Name}] {msg}");
+            }
+
+            if (Debugger.IsAttached) Log("[Debug] Found world seed: " + WorldGen.currentWorldSeed);
+
+            if (!int.TryParse(WorldGen.currentWorldSeed, out int seed)) seed = Crc32.Calculate(WorldGen.currentWorldSeed);
+
             Log($"Loading recipe cache for world seed: {seed}");
 
             if (Shuffler.Caches.ContainsKey(seed))
@@ -27,13 +70,13 @@ namespace RecipeShuffler
                     Log("Failed to verify pre-existing recipe cache for world, going to re-shuffle.");
                     goto IntegFail;
                 }
-                
+
                 Log("Using verified pre-existing recipe cache for world.");
-                
+
                 return;
             }
-            
-            IntegFail:
+
+        IntegFail:
             RecipeCache cache = new();
             cache.SetRecipes(Shuffler.VanillaCache.ReadonlyRecipes);
             cache.ShuffleRecipes(seed);
@@ -43,12 +86,12 @@ namespace RecipeShuffler
                 Log("Couldn't verify integrity of brand-new recipe cache, panicking!");
                 return;
             }
-            
+
             Log("Successfully shuffled and verified newly-created recipe cache.");
-            
+
             Shuffler.VanillaCache.SetRecipes(cache.ReadonlyRecipes);
             Shuffler.Caches[seed] = cache;
-            
+
             Log("Applied new recipe cache to the current world.");
         }
     }
